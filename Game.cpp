a2734e2d14fc59e3a3e3916b9014974dc40195f9
scardydragon49 <scardydragon49@gmail.com>
2026-06-01 19:52:31 -0600

    Created vector of bricks and adding bricks to vector

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,19 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	brick.width = 10;
-	brick.height = 2;
-	brick.x_position = 0;
-	brick.y_position = 5;
-	brick.doubleThick = true;
-	brick.color = ConsoleColor::DarkGreen;
+
+	for (int i = 0; i < 5; i++) {
+		Box tempBrick;
+
+		tempBrick.width = 10;
+		tempBrick.height = 2;
+		tempBrick.x_position = i*10;
+		tempBrick.y_position = 5;
+		tempBrick.doubleThick = true;
+		tempBrick.color = ConsoleColor::DarkGreen;
+
+		bricks.push_back(tempBrick);
+	}	
 }
 
 void Game::ResetBall()
--- Game.h
+++ Game.h
@@ -2,12 +2,15 @@
 #include "Box.h"
 #include "Ball.h"
 
+#include <vector>
+
 class Game
 {
 	Ball ball;
 	Box paddle;
 
 	// TODO #1 - Instead of storing 1 brick, store a vector of bricks (by value)
+	std::vector<Box> bricks;
 	Box brick;
 
 public:
