b3dc11d54e7511cedac97fd1cc89b964834acfd4
scardydragon49 <scardydragon49@gmail.com>
2026-06-01 20:03:26 -0600

    Updated collision to work with vector

--- Game.cpp
+++ Game.cpp
@@ -86,13 +86,20 @@ void Game::Render() const
 void Game::CheckCollision()
 {
 	// TODO #4 - Update collision to check all bricks
-	if (brickOLD.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
-	{
-		brickOLD.color = ConsoleColor(brickOLD.color - 1);
-		ball.y_velocity *= -1;
 
-		// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
+	/*
+	I know its for an assaignment and all but
+	wouldnt doing collision checks with the ball be better
+	*/
+	for (Box& brick : bricks) {
+		if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+		{
+			brick.color = ConsoleColor(brick.color - 1);
+			ball.y_velocity *= -1;
 
+			// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
+
+		}
 	}
 
 	// TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
